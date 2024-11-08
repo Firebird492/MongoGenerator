8cfdb1927b17475dc44b2266e9d65cbb0cdd1db1
Firebird492 <gavinbuier@outlook.com>
2024-11-08 16:19:51 -0500

    add a noDelete field to not add the Delete endpoint

--- service.py
+++ service.py
@@ -3,11 +3,12 @@ class Field:
     def __init__(self, json: dict) -> None:
         self.name = json["name"]
         self.isRequired = json.get("isRequired", False)
-        
+
 
 class Table:
     def __init__(self, json: dict) -> None:
         self.name = json["name"]
+        self.noDelete = json.get("noDelete", False)
         self.fields = []
         for f in json["fields"]:
             self.fields.append(Field(f))
@@ -17,6 +18,9 @@ class Table:
     
     def getFields(self):
         return self.fields
+    
+    def excludeDelete(self):
+        return self.noDelete
     
     def getRequiredFields(self):
         return [f for f in self.fields if f.isRequired]
