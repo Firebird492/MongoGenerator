2afabb47709c4191f69e29c8befefbc05577c876
Firebird492 <gavinbuier@outlook.com>
2024-11-12 16:00:44 -0500

    add models.js file generation

--- generate.py
+++ generate.py
@@ -14,7 +14,14 @@ with open('inputs/tmc.json', 'r') as file:
 
 service = Service(jsonData)
 env = Environment(loader = FileSystemLoader('templates'))
+# mongo functions
 template = env.get_template('MongoFunctionsTemplate.jinja')
 output = template.render(Service = service)
 with open("renders/outputFileName.js", 'w') as f:
+    print(output, file = f)
+
+# models
+template = env.get_template('MongoModelsTemplate.jinja')
+output = template.render(Service = service)
+with open("renders/models.js", 'w') as f:
     print(output, file = f)
--- service.py
+++ service.py
@@ -3,18 +3,43 @@ class Field:
     def __init__(self, json: dict) -> None:
         self.name = json["name"]
         self.isRequired = json.get("isRequired", False)
+        self.type = json.get("type", "NA")
+
+        defaults = {
+            "str": "''",
+            "int": -1,
+            "float": -1,
+            "array": "[]",
+            "bool": "false",
+            "NA": "null",
+        }
+        self.default = defaults[self.type] 
+
+    def getName(self):
+        return self.name
+    
+    def getDefault(self):
+        return self.default
+
 
 
 class Table:
     def __init__(self, json: dict) -> None:
         self.name = json["name"]
+
+        if "plural" not in self.name:
+            self.name["plural"] = self.name["name"] + "s"
+
         self.noDelete = json.get("noDelete", False)
         self.fields = []
         for f in json["fields"]:
             self.fields.append(Field(f))
 
     def getName(self):
-        return self.name
+        return self.name["name"]
+    
+    def getPluralName(self):
+        return self.name["plural"]
     
     def getFields(self):
         return self.fields
